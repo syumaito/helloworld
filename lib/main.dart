c399ba8ea8ba4c2eca5154bfd57ad84d3900e3fa
DESKTOP-KNAIK21\shuma-ito <sssk1202syu@gmail.com>
2025-02-23 01:09:36 +0900

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+      home: TestPage(),
+    );
+  }
+}
+
+ class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Test App'),
+      ),
+      body: const Text('Hello World'),
+    );
+  }
+}
